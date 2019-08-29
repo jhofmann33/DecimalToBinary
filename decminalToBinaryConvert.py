18f61696199b47d78437a2602f6b3f18700adf5b
John Hofmann <46803841+jhofmann33@users.noreply.github.com>
2019-08-29 11:06:38 -0500

    uploaded file
    actual file .py file to download and run

---
+++ decminalToBinaryConvert.py
@@ -0,0 +1,23 @@
+#Binary Test
+
+#Run and type "main(your decimal value here)" and hit enter
+#doesnt work with fractions yet.
+def main(decimal):
+    r = 0
+    n = decimal
+    result = ""
+    remainder = 0
+    while((2**r) <= n):
+        r = r+ 1
+        #print(r)
+    print("amount of bits is " + str(r))
+    while(n != 0):
+        remainder = int(n % 2)
+        #print(remainder)
+        n = int(n / 2)
+        #print(n)
+        result = str(remainder)+ result 
+        print(result)
+                            
+    
+    
